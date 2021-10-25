79cf57a431b6b297484996002b181c634840f672
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-10-25 20:17:18 -0300

    Writing to a text file

--- files/src/files/Files.java
+++ files/src/files/Files.java
@@ -3,10 +3,34 @@ package files;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Files {
 	
 	public static void main(String[] args) throws IOException {
+		
+		Person person1 = new Person();
+		person1.setName("Wyltamar Douglas");
+		person1.setEmail("wyltamar@hotmail.com");
+		person1.setAge(38);
+		
+		Person person2 = new Person();
+		person2.setName("Amanda Gonçalves");
+		person2.setEmail("amaga.jerico@hotmail.com");
+		person2.setAge(34);
+		
+		Person person3 = new Person();
+		person3.setName("Pedro Lucas");
+		person3.setEmail("pedro123@gmail.com");
+		person3.setAge(11);
+		
+		List<Person> persons = new ArrayList<Person>();
+		
+		persons.add(person1);
+		persons.add(person2);
+		persons.add(person3);
+		
 		
 		// Creating an instance of File and passing pathname parameter
 		File file = new File("C:\\Users\\wylta\\git\\repository2\\files\\src\\files\\file.txt");
@@ -20,9 +44,8 @@ public class Files {
 		FileWriter fileWriter = new FileWriter(file);
 		
 		
-		for(int i = 1; i <= 20; i++) {
-			
-			fileWriter.write("My text of row "+i+" \n"); //to write in the file 
+		for (Person person : persons) {
+			fileWriter.write(person.getName() + ";" + person.getEmail() + ";" + person.getAge() + "\n");
 		}
 		
 		fileWriter.flush(); // persists the file 
